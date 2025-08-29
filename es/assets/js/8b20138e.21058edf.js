01bd90659cebfade84138189e063894d89e52f05
marqbeniamin <marqbeniamin@users.noreply.github.com>
2025-08-29 13:15:52 +0000

    deploy: 39372a60d6310f36364efcf5f0c4a92b245f7449

--- es/assets/js/8b20138e.21058edf.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkgalio=self.webpackChunkgalio||[]).push([[2058],{5140:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/es/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
